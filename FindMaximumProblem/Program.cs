68f8639ae022db55388117dd9a313b3fd612c771
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-05-11 18:30:06 +0530

    [Sandeep] Adding welcome message

---
+++ FindMaximumProblem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Find maximum problem using generics");
+
+            Console.ReadLine();
+        }
+    }
+}
